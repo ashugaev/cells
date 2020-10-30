0050531c861c406a346127d5fac2a068c15f83f8
Шугаев Алексей <a.shugaev@corp.mail.ru>
2020-10-30 13:02:40 +0300

    добавил компонент кнопки

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import styles from './Button.module.sass';
+import Text, {TextColors, TextSizes} from "../Text";
+import classnames from 'classnames';
 
-function Button() {
+interface ButtonProps {
+    text: string;
+    className?: string;
+}
+
+const Button: React.FC<ButtonProps> = ({text, className}) => {
+    const buttonClassName = classnames(styles.button, className)
+
     return (
-        <div>
-            kek
-        </div>
+        <button className={buttonClassName}>
+            <Text size={TextSizes.s} text={text} color={TextColors.white} />
+        </button>
     );
 }
 
