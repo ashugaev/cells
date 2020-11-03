975bd662e19c5e11d7d8ae72a77643b7d5918256
Aleksey Shugaev <alek-2020@yandex.ru>
2020-11-03 17:54:30 +0300

    вынес хук

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,35 +1,15 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect} from 'react';
 import styles from "./App.module.sass";
 import Button from "../Button";
 import Text, {TextColors, TextSizes} from "../Text";
 import CellsList from "../CellsList";
-import {CellData, CellTypes} from '../Cell';
-import {cellsByTypes} from './utils';
+import {useCells} from "./hooks";
 
-function killCellIfLife(arr: CellData[], index: number): void {
-    if(arr[index] && arr[index].type === CellTypes.life) {
-        arr = arr.slice(0, index - 1).concat(arr.slice(index));
-        // Рекурсивная проверка на то, что радом с этой жизнь не было еще одной
-        killCellIfLife(arr, index - 1);
-    }
-};
-
-function App() {
-    const [cells, setCells] = useState<CellData[]>([])
+const App: React.FC = () => {
     const contentRef = React.useRef<HTMLDivElement>(null);
     const appRef = React.useRef<HTMLDivElement>(null);
 
-    const addRandomCell = () => {
-        const randomVal = Math.round(Math.random());
-
-        const newCell = randomVal ? cellsByTypes[CellTypes.dead]() : cellsByTypes[CellTypes.alive]();
-
-        const arrWithNewCell = [...cells, newCell];
-
-        const validatedCells = validateCells(arrWithNewCell);
-
-        setCells(validatedCells);
-    }
+    const [cells, addCell] = useCells();
 
     function scrollDown() {
         if (contentRef.current && appRef.current) {
@@ -38,39 +18,6 @@ function App() {
                 behavior: "smooth"
             });
         }
-    }
-
-    const validateCells = (arrWithNewCell: CellData[]) => {
-        if (arrWithNewCell.length < 2) return arrWithNewCell;
-
-        const sameCellsCounter:{type: CellTypes | null, count: number} = {
-            type: null,
-            count: 0,
-        };
-
-        const validatedCellsArr = arrWithNewCell.reduce((acc: CellData[], cell: CellData) => {
-            acc.push(cell);
-
-            if(sameCellsCounter.type !== cell.type) {
-                if(sameCellsCounter.type === CellTypes.alive && sameCellsCounter.count === 2) {
-                    acc = acc.slice(0, -2);
-                    acc.push(cellsByTypes[CellTypes.life]());
-                }
-
-                if(sameCellsCounter.type === CellTypes.dead && sameCellsCounter.count === 3) {
-                    killCellIfLife(acc, acc.length - 4)
-                }
-
-                sameCellsCounter.type = cell.type;
-                sameCellsCounter.count = 1;
-            } else {
-                sameCellsCounter.count++;
-            }
-
-            return acc;
-        }, [])
-
-        return validatedCellsArr;
     }
 
     useEffect(() => {
@@ -97,7 +44,7 @@ function App() {
                 <Button
                     text="СОТВОРИТЬ"
                     className={styles.button}
-                    onClick={addRandomCell}
+                    onClick={addCell}
                 />
             </div>
         </div>
