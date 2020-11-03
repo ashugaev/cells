5ce2d0d21167e31403da4713480a2864d9ef5a9b
Aleksey Shugaev <alek-2020@yandex.ru>
2020-11-03 16:55:58 +0300

    плейсхолдер в списке клеток

--- src/components/CellsList/CellsList.tsx
+++ src/components/CellsList/CellsList.tsx
@@ -1,19 +1,33 @@
 import React from 'react';
 import Cell, {CellData} from "../Cell";
 import styles from './CellsList.module.sass';
+import classnames from 'classnames';
+import Text, {TextColors} from '../Text';
 
 interface CellsListProps {
     cells: CellData[];
+    className?: string;
 }
 
-const CellsList: React.FC<CellsListProps> = ({cells}) => {
-    console.log(styles)
+const CellsList: React.FC<CellsListProps> = ({cells, className}) => {
+    const cellsListClassName = classnames(className, styles.cellsList);
+
+    if(!cells.length) {
+        return (
+            <Text
+                className={styles.placeholder}
+                text='Для начала процесса зарождения нажмите "СОТВОРИТЬ"'
+                transparent
+                color={TextColors.white}
+             />
+        )
+    }
     return (
-        <>
-            {cells.map(data => (
-                    <Cell {...data} className={styles.cell}/>
-            ))}
-        </>
+        <div className={cellsListClassName}>
+            {cells.map((data) => (
+                    <Cell key={data.id} {...data} className={styles.cell}/>
+                    ))}
+        </div>
     );
 }
 
